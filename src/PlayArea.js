b3ca2c7b65cf880b892a2af64eb8a9581bc68379
ericbates <eric.steven.bates@gmail.com>
2022-02-16 20:10:34 -0500

    output dummy words in PlayArea

--- src/PlayArea.js
+++ src/PlayArea.js
@@ -3,11 +3,19 @@ import Row from './Row';
 //The main play area
 //Displays current input line and previous guesses
 const PlayArea = () => {
+  //TEMPORARY DUMMY DATA
+  const previousGuesses = ['was', 'one', 'two', 'four', 'tear'];
+
+  const rows = [];
+  previousGuesses.forEach(guess => {
+    rows.push(
+      <Row word={guess} key={guess} />
+    );
+  });
+
   return (
     <div className="play-area">
-      <Row />
-      <Row />
-      <Row />
+      {rows}
     </div>
   );
 };
--- src/Row.js
+++ src/Row.js
@@ -1,17 +1,26 @@
-const Letter = () => {
+const Letter = (props) => {
+  const letter = props.value;
   return (
     <div className="letter">
-      <p>A</p>
+      <p>{letter}</p>
     </div>
   );
 };
 
-const Row = () => {
+const Row = (props) => {
+  const word = props.word;
+
+  //create a Letter component for each letter in the word
+  const letters = [];
+  [...word].forEach(letter => {
+    letters.push(
+      <Letter value={letter} key={letter} />
+    );
+  });
+
   return (
     <div className="row">
-      <Letter />
-      <Letter />
-      <Letter />
+      {letters}
     </div>
   );
 };
