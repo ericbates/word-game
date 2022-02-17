cff4c554abf803ee74de2e076f38e72a7d9fa2d9
ericbates <eric.steven.bates@gmail.com>
2022-02-16 20:39:30 -0500

    output Stats info

--- src/Stats.js
+++ src/Stats.js
@@ -5,8 +5,21 @@ const Stats = (props) => {
   const numGuesses = props.numGuesses;
   const correctGuesses = props.correctGuesses;
 
+  const answers = [];
+  correctGuesses.forEach(guess => {
+    answers.push(
+      <p>{guess}</p>
+    );
+  });
+
   return (
     <div className="stats-bar">
+      <div className="correct-guesses">
+        {answers}
+      </div>
+      <div className="guess-stats">
+        <p>{numGuesses}/{totalGuesses}</p>
+      </div>
     </div>
   );
 };
