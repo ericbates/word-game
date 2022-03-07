4daa5e0c1fef3a9ac6465fc901e2c20ac750e17a
ericbates <eric.steven.bates@gmail.com>
2022-03-07 15:36:31 -0500

    add outline of keydown event listener

--- src/WordGame.js
+++ src/WordGame.js
@@ -1,14 +1,31 @@
+import { useEffect } from 'react';
 import ProgressArea from './ProgressArea/ProgressArea';
 import PlayArea from './PlayArea/PlayArea';
 import KeyboardArea from './KeyboardArea/KeyboardArea';
 import "./css/WordGame.css";
 import './css/StatusColors.css';
+import React from 'react';
 
 const WordGame = () => {
   //TEMPORARY DUMMY DATA 
   const totalGuesses = 20;
   let numGuesses = 10;
   const foundAnswers = ['two', 'tear', 'index', 'apples', 'jukebox', 'aaaaaaaa'];
+
+  //listen for keystrokes
+  useEffect (() => {
+    const handleKeyDown = (event) => {
+      //check if pressed key is a single letter
+      if(/^[a-zA-Z]{1}$/.test(event.key)){
+        
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
 
   return (
     <div className="word-game-app">
