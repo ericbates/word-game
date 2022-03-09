69f95341b759f4cd6677d464c2d66f57ab509232
ericbates <eric.steven.bates@gmail.com>
2022-03-09 16:44:54 -0500

    implement typing capabilities in KeyboardArea

--- src/PlayArea/KeyboardArea/KeyboardArea.js
+++ src/PlayArea/KeyboardArea/KeyboardArea.js
@@ -1,18 +1,56 @@
-import Keyboard from './Keyboard';
 import './KeyboardArea.css';
+const statuses = ['absent', 'misplaced', 'correct', 'default'];
 
-//Displays the Keyboard area
-//Used to input letters into PlayArea
-const KeyboardArea = () => {
-  //the keys for the keyboard
+//displays the interactive keyboard
+const KeyboardArea = (props) => {
   const keys = [
     ['q','w','e','r','t','y','u','i','o','p'],
     ['a','s','d','f','g','h','j','k','l'],
     ['enter','z','x','c','v','b','n','m','del']
   ];
 
+  const rows = keys.map((keyRow, rowIndex) => {
+    return (
+      //create a 'keyboard-row' div for each row of keys (3)
+      <div className='keyboard-row' key={`keyboard-row-${rowIndex}`}>
+        {keyRow.map((key, keyIndex) => {
+          key = key.toUpperCase();
+          //TEMPORARY for styling purposes
+          let status = statuses[Math.floor(Math.random() * 4)];
+          if(key === 'ENTER' || key === 'DEL') {
+            status = 'default';
+          }
+
+          //set className and onClick function for each key
+          let className = 'keyboard-key';
+          let onClick = props.typeLetter;
+          if(key === 'ENTER') {
+            className += ' ent';
+            onClick = props.submitGuess;
+          } else if(key === 'DEL') {
+            className += ' del';
+            onClick = props.deleteLetter;
+          }
+
+          return (
+            //build a 'keyboard-key' div for each key in the row
+            <div
+              className={`${className} ${status}`}
+              onClick={onClick}
+              key={`keyboard-key-${rowIndex},${keyIndex}`}
+            >
+              <h3>{key.toUpperCase()}</h3>
+            </div>
+          )
+        })}
+      </div>
+    )
+  })
+
   return (
-    <Keyboard keys={keys} />
+    <section id='keyboard-area'>
+      {rows}
+    </section>
   )
 }
 
--- src/PlayArea/PlayArea.js
+++ src/PlayArea/PlayArea.js
@@ -5,26 +5,38 @@ import CurrentGuess from './CurrentGuess';
 import './PlayArea.css';
 
 //The main play area
-//Displays current input line and previous guesses
-//Displays KeyboardArea as child component
+//manages currentGuess state and adds to previousGuess state upon submit
+//parent component of PreviousGuesses, CurrentGuesses, and KeyboardArea
 const PlayArea = (props) => {
   const [currentGuess, setCurrentGuess] = useState('');
 
+  //length of current word, increases by 1 everytime a new word is found
   const guessLength = props.foundAnswers.length + 3;
+  
   const setPreviousGuesses = props.setPreviousGuesses;
 
+  //recieves a single character and adds it to the current guess
   const typeLetter = useCallback((letter) => {
     if(currentGuess.length < guessLength) {
       setCurrentGuess(prevCurrentGuess => prevCurrentGuess.concat(letter));
     }
   }, [currentGuess, guessLength])
 
+  //finds the letter from keyboard-key div and calls typeLetter
+  const typeLetterKeyboard = useCallback((event) => {
+    const letter = event.currentTarget.getElementsByTagName('h3')[0].innerHTML;
+    typeLetter(letter);
+  }, [typeLetter])
+
+
+  //removes most recently added character from the current guess
   const deleteLetter = useCallback(() => {
     if(currentGuess.length > 0) {
       setCurrentGuess(prevCurrentGuess => prevCurrentGuess.substring(0, prevCurrentGuess.length - 1));
     }
   }, [currentGuess])
 
+  //submits the current guess to be added to the previousGuesses state
   const submitGuess = useCallback(() => {
     if(currentGuess.length === guessLength) {
       setPreviousGuesses(prevPreviousGuesses => [...prevPreviousGuesses, currentGuess]);
@@ -59,7 +71,7 @@ const PlayArea = (props) => {
           <CurrentGuess guess={currentGuess} guessLength={guessLength}/>
         </div>
       </section>
-      <KeyboardArea />
+      <KeyboardArea typeLetter={typeLetterKeyboard} deleteLetter={deleteLetter} submitGuess={submitGuess} />
     </>
   )
 }
