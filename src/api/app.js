a4a544490473b927b9c38100386e7962f5575e24
ericbates <eric.steven.bates@gmail.com>
2022-04-18 13:37:29 -0400

    initialize testapi

--- src/api/app.js
+++ src/api/app.js
@@ -7,6 +7,7 @@ var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var testRouter = require('./routes/testApi');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/testapi', testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
